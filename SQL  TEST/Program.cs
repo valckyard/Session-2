ce3ba1fbe1ac32fda16199d025e42bdaecea301e
Alexandre Caron <alexcaron880@gmail.com>
2018-02-06 10:43:13 -0500

    ok done

--- SQL  TEST/Program.cs
+++ SQL  TEST/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SqlTest_CSharp
 {
@@ -12,47 +13,16 @@ namespace SqlTest_CSharp
     {
         static void Main(string[] args)
         {
-
-            using (SqlConnection TODOSqlConn = new SqlConnection())
+            string OneLine;
+            List<string> Noms = new List<string>();
+            StreamReader SanchoBobReadsText = new StreamReader("names.txt"); //32868 names that 5 or more baby had been given in USA in 2016
+            while ((OneLine = SanchoBobReadsText.ReadLine()) != null)
             {
-              
-                TODOSqlConn.ConnectionString = "user id = sa; " +
-                                      "password=Blade880;server=localhost;" +//174.94.71.98
-                                      "Trusted_Connection=yes;" +
-                                      "database=TODODB; " +
-                                      "connection timeout=30"; 
-               
-    
-                try
-                {
-                    TODOSqlConn.Open();
-                    SqlCommand command = new SqlCommand("SELECT * FROM username", TODOSqlConn);
-                    SqlCommand insertCommand = new SqlCommand("alter table usernameaaa add suif varchar(30)", TODOSqlConn);
-
-                    Console.WriteLine("Commands executed! Total rows affected are " + insertCommand.ExecuteNonQuery());
-
-                }
-                catch (SqlException er)
-                {
-                    Console.WriteLine("There was an error reported by SQL Server, " + er.Message);
-                }
-                TODOSqlConn.Close();
-                TODOSqlConn.Dispose();
-
-                string dbname = "SUIF";
-
-                SqlCommand CreateDB = new SqlCommand();
-
-                CreateDB.CommandText = "TODO_DB_BuildTable";
-                CreateDB.Parameters.Clear();
-                CreateDB.CommandType = CommandType.StoredProcedure;
-                CreateDB.Parameters.AddWithValue("@TableName", dbname);
-                //CreateDB.Parameters.AddWithValue("@YourCustomTable", dtCustomerFields).SqlDbType = SqlDbType.Structured;
-
-                //Execute
-                CreateDB.ExecuteNonQuery();
-
+                string[] SplittedLine = OneLine.Split(',');             //splitting at , i dont want the rest
+                Noms.Add(SplittedLine[0]);
             }
+           StreamWriter  new str
         }
+          
     }
 }
